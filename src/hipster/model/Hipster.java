a2f8721d048f0ff4a0830b030746bc2a9ca1a28c
Colm Laro <smartman97@gmail.com>
2015-11-19 08:48:06 -0700

    Set up the layout for the gui

--- src/hipster/model/Hipster.java
+++ src/hipster/model/Hipster.java
@@ -6,6 +6,59 @@ public class Hipster
 	private String [] hipsterPhrases;
 	private Book [] hipsterBooks;
 	
+	public Hipster()
+	{
+		this.name = "";
+		this.hipsterPhrases = new String[4];
+		this.hipsterBooks = new Book[3];
+		
+		setupArray();
+		setupBooks();
+	}
+	
+	private void setupArray()
+	{
+		hipsterPhrases[0] = "This is so mainstream!";
+		hipsterPhrases[1] = "I don't mean sound like a hipster, but, ";
+		hipsterPhrases[2] = "Insert sarcastic phrase here";
+		hipsterPhrases[3] = "I was into code before it was cool";
+	}
+	
+	private void setupBooks()
+	{
+		Book firstBook, secondBook, thirdBook;
+		
+		firstBook = new Book();
+		firstBook.setAuthor("Andy Weir");
+		firstBook.setTitle("The Martian");
+		firstBook.setSubject("Science Fiction");
+		firstBook.setPageCount(369);
+		firstBook.setPrice(14.88);
+		
+		secondBook = new Book();
+		secondBook.setAuthor("Orson Scott Card");
+		secondBook.setTitle("Enders Game");
+		secondBook.setSubject("Science Fiction");
+		secondBook.setPageCount(384);
+		secondBook.setPrice(5.99);
+		
+		thirdBook = new Book(374, "Suzanne Collins", "The Hunger Games", "Dystopain", 6.99);
+		
+		hipsterBooks[0] = firstBook;
+		hipsterBooks[1] = secondBook;
+		hipsterBooks[2] = thirdBook;
+	}
+	
+	public Book[] getHipsterBooks()
+	{
+		return hipsterBooks;
+	}
+
+	public void setHipsterBooks(Book[] hipsterBooks)
+	{
+		this.hipsterBooks = hipsterBooks;
+	}
+
 	public String getName()
 	{
 		return name;
@@ -24,22 +77,5 @@ public class Hipster
 	public void setHipsterPhrases(String[] hipsterPhrases)
 	{
 		this.hipsterPhrases = hipsterPhrases;
-	}
-
-	public Hipster()
-	{
-		this.name = "";
-		this.hipsterPhrases = new String[4];
-		
-		setupArray();
-		
-	}
-	
-	private void setupArray()
-	{
-		hipsterPhrases[0] = "This is so mainstream!";
-		hipsterPhrases[1] = "I don't mean sound like a hipster, but, ";
-		hipsterPhrases[2] = "Insert sarcastic phrase here";
-		hipsterPhrases[3] = "I was into code before it was cool";
 	}
 }
--- src/hipster/view/HipsterPanel.java
+++ src/hipster/view/HipsterPanel.java
@@ -1,11 +1,13 @@
 package hipster.view;
 
 import hipster.controller.HipsterController;
+import hipster.model.Book;
 
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
+
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -32,11 +34,38 @@ public class HipsterPanel extends JPanel
 		this.baseController = baseController;
 		baseLayout = new SpringLayout();
 		phraseComboBox = new JComboBox<String>();
-
+		bookPageCountLabel = new JLabel("The page count");
+		bookAuthorLabel = new JLabel("The author");
+		bookPriceLabel = new JLabel("The price");
+		bookSubjectLabel = new JLabel("The subject");
+		bookTitleLabel = new JLabel("The title");
+		changeBookButton = new JButton("Change books");
+		maxClicks = baseController.getFirstHipster().getHipsterBooks().length;
+		startClick = 0;
+		
 		setupComboBox();
 		setupPanel();
 		setupLayout();
 		setupListeners();
+	}
+	
+	private void setupLayout()
+	{
+		baseLayout.putConstraint(SpringLayout.WEST, bookPageCountLabel, 0, SpringLayout.WEST, bookAuthorLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, bookAuthorLabel, 55, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, bookPriceLabel, 19, SpringLayout.SOUTH, bookPageCountLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, bookPriceLabel, 0, SpringLayout.WEST, bookAuthorLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, bookPageCountLabel, 22, SpringLayout.SOUTH, bookSubjectLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, bookSubjectLabel, 19, SpringLayout.SOUTH, bookAuthorLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, bookSubjectLabel, 0, SpringLayout.WEST, bookAuthorLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, bookAuthorLabel, 14, SpringLayout.SOUTH, bookTitleLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, bookTitleLabel, 58, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, phraseComboBox, 92, SpringLayout.EAST, changeBookButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, bookTitleLabel, 22, SpringLayout.SOUTH, changeBookButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, phraseComboBox, 1, SpringLayout.NORTH, changeBookButton);
+		baseLayout.putConstraint(SpringLayout.WEST, phraseComboBox, 6, SpringLayout.EAST, changeBookButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, changeBookButton, 10, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, changeBookButton, 98, SpringLayout.WEST, this);
 	}
 
 	/**
@@ -60,11 +89,32 @@ public class HipsterPanel extends JPanel
 				baseController.getBaseFrame().setTitle(updatedTitle);
 			}
 		});
-	}
-
-	private void setupLayout()
-	{
 		
+		changeBookButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				Book [] tempBooks = baseController.getFirstHipster().getHipsterBooks();
+				if(startClick < maxClicks)
+				{
+					bookSubjectLabel.setText("Book Subject: " + tempBooks[startClick].getSubject());
+					bookAuthorLabel.setText("Book Author: " + tempBooks[startClick].getAuthor());
+					bookTitleLabel.setText("Book Title: " + tempBooks[startClick].getTitle());
+					bookPageCountLabel.setText("Book Pages: " + tempBooks[startClick].getPageCount());
+					bookPriceLabel.setText("Book Price: " + tempBooks[startClick].getPrice());
+					startClick++;
+				}
+				else
+				{
+					startClick = 0;
+					bookSubjectLabel.setText("Book subject: ");
+					bookAuthorLabel.setText("Book Author: ");
+					bookTitleLabel.setText("Book Title: ");
+					bookPageCountLabel.setText("Book Pages: ");
+					bookPriceLabel.setText("Book Price: ");
+				}
+			}
+		});
 	}
 
 	private void setupPanel()
@@ -72,6 +122,12 @@ public class HipsterPanel extends JPanel
 		this.setLayout(baseLayout);
 		this.setBackground(Color.ORANGE);
 		this.add(phraseComboBox);
+		this.add(bookAuthorLabel);
+		this.add(bookPageCountLabel);
+		this.add(bookTitleLabel);
+		this.add(bookPriceLabel);
+		this.add(bookSubjectLabel);
+		this.add(changeBookButton);
 	}
 	
 	private Color randomColor()
